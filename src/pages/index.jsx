a04e3336ee1b0f88d2a7f602038f27be89b77295
Riju Mukherjee <rmukh561@gmail.com>
2021-02-24 15:41:11 +0530

    💡 Added comments

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,20 +4,30 @@ import styles from "./index.module.css"
 const Main = () => {
   const [time, settime] = useState(null)
   const [date, setDate] = useState(null)
+
+  //Sets the current time
   const setHandler = () => {
     settime(new Date().toLocaleTimeString())
   }
+
+  //Sets the current date
   const dateHandler = () => {
     setDate(new Date().toDateString())
   }
+
+  //Runs the function as the component mounts
   useEffect(() => {
     setHandler()
     dateHandler()
   }, [])
+
+  //Updates the time every 1 second
   const f = setInterval(setHandler, 1000)
+
   return (
     <div className={styles.div}>
       <div className={styles.t}>
+        {/* Displays the date and time */}
         <p className={styles.d}>{date}</p>
         <p>{time}</p>
         <p className={styles.h}>
